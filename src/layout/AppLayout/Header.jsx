f4e7361f733425511b5d85101759dd1355031488
JooEonKim <james5459@naver.com>
2026-02-07 12:20:29 +0900

    Remove center logo and title from header

--- src/layout/AppLayout/Header.jsx
+++ src/layout/AppLayout/Header.jsx
@@ -7,12 +7,7 @@ export default function Header() {
   const dispatch = useDispatch();
   return (
     <header className={styles.header}>
-      <div className={styles.logoBox}>
-        <div className={styles.logoMark} />
-        <div className={styles.logoText}>TM 업무</div>
-      </div>
 
-      <div className={styles.centerTitle}>샤인유의원 고객관리팀</div>
 
       <div className={styles.right}>
         <div className={styles.tm}>{tm}</div>
